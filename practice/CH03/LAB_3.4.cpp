3a360eff0e793da9bab2c7b2a6ef386fb9269f2a
Young <amzoy1234@naver.com>
2026-03-03 21:21:10 +0900

    fix : LAB_3.4.cpp

--- practice/CH03/LAB_3.4.cpp
+++ practice/CH03/LAB_3.4.cpp
@@ -2,13 +2,19 @@
 void show(int a);
 int main() {
   int a;
+  std::cout << " 알파벳의 ASCII 코드값을 입력하시오(65~90, 97~122): ";
   std::cin >> a;
   show(a);
   return 0;
 }
 
 void show(int a) {
-  std::cout << a;
-  std::cout << ",   " << static_cast<char>(a);
-  std::cout << ",   " << static_cast<char>(toupper(a));
+  if ((a >= 65 && a <= 90) || (a >= 97 && a <= 122)) {
+    std::cout << a;
+    std::cout << ",   " << static_cast<char>(a);
+    std::cout << ",   " << static_cast<char>(toupper(a));
+  } else {
+    std::cout << "입력한 ASCII 코드값은 알파벳이 아닙니다." << std::endl;
+    return;
+  }
 }
